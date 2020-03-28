a0f5e0233c6d7e320fb87cdf6dfc59e2cc9aff7d
Ivan Shavliuga <ivanov-R0M@yandex.ru>
2020-03-28 16:25:42 +0300

    panel html tags

--- src/components/text.vue
+++ src/components/text.vue
@@ -12,16 +12,30 @@
                     this.sendMessage(this.content);
                     this.content = '';
                 }
+                if (e.keyCode === 13 && this.content.length) {
+                    this.content = this.content+'<br>';
+                }
             },
             ...mapMutations([
                 'sendMessage'
-            ])
+            ]),
+            addelem(tag) {
+                this.content = this.content + " <" +tag+">  "+"</"+tag+">";             
+            }
         }
     };
 </script>
 
 <template>
     <div class="text">
+        <div class="menubar">
+            <button @click="addelem('b')" title="bold">B</button>
+            <button @click="addelem('i')" title="italic">I</button>
+            <button @click="addelem('u')" title="underline">U</button>
+            <button @click="addelem('a')" title="link">A</button>
+            <button @click="addelem('pre')" title="code">PRE</button>
+            <button @click="addelem('q')" title="quote">Q</button>
+        </div>
         <textarea placeholder="Ctrl + Enter" v-model="content" @keyup="onKeyup"></textarea>
     </div>
 </template>
@@ -32,12 +46,25 @@
         border-top: solid 2px #eee;
         textarea {
             padding: 10px;
-            height: 100%;
+            height: 90%;
             width: 100%;
             border: none;
             outline: none;
             font-family: "Micrsofot Yahei";
+            font-size: 14px;
             resize: none;
+        }
+        .menubar{
+          background:#eee;
+          padding:4px 10px;
+          button {
+            height: 10%;
+            font-size:14px;
+            border-radius:7px;
+            background: #ccc;
+            border:1px solid #444;
+            font-weight: bold;
+          }        
         }
     }
 </style>
